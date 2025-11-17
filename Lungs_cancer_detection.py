52722453dcce328186f3432b325231b72680fbde
harsh pandya <harsh.pandya@vrinsoft.com>
2025-11-17 10:34:00 +0530

    Updated

---
+++ Lungs_cancer_detection.py
@@ -0,0 +1,20 @@
+import pandas as pd 
+import numpy as np 
+import matplotlib as plt
+from PLt import Image 
+from glob import glob
+
+from sklearn.model_selection import train_test_split 
+from sklearn import metrics
+
+from zipfile import ZipFile
+import cv2 
+import gc
+import os
+
+import tensorflow as tf 
+from tensorflow import keras
+from keras import layers 
+
+import warnings
+warnings.filterwarnings('ignore')
