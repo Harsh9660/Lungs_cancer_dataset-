18d308716f76263374586a615021ea22b5ffec9e
harsh pandya <harsh.pandya@vrinsoft.com>
2025-11-17 11:08:23 +0530

    Updated

---
+++ Train_Testmodel_lungs.py
@@ -0,0 +1,14 @@
+from sklearn.model_selection import train_test_split
+import os 
+import shutil
+import time
+
+class FileEventHandler(FileSystemEventHandler):
+
+            train_test_split = 0.8
+            X_train, X_test = X_train, 
+            X_test = train_test_split
+            y_train, y_test = 
+
+
+            
